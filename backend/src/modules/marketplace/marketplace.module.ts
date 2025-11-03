e3efb7df42855e3d8ac63d5c3c830ff735814cf0
kamja44 <88872409+kamja44@users.noreply.github.com>
2025-11-03 09:13:37 +0900

    product schema Module에 등록

--- backend/src/modules/marketplace/marketplace.module.ts
+++ backend/src/modules/marketplace/marketplace.module.ts
@@ -1,6 +1,8 @@
 import { Module } from '@nestjs/common';
 import { MarketplaceController } from './marketplace.controller';
 import { MarketplaceService } from './marketplace.service';
+import { MongooseModule } from '@nestjs/mongoose';
+import { Product, ProductSchema } from './schemas/product.schema';
 
 /**
  * MarketplaceModule - 중고거래 모듈
@@ -8,6 +10,7 @@ import { MarketplaceService } from './marketplace.service';
  * - 거래 채팅
  */
 @Module({
+  imports: [MongooseModule.forFeature([{ name: Product.name, schema: ProductSchema }])],
   controllers: [MarketplaceController],
   providers: [MarketplaceService],
 })
